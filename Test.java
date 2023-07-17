71d612339c0b1899065a036c90bc005d7c26c6aa
Sravani-Rowthu <138876002+Sravani-Rowthu@users.noreply.github.com>
2023-07-17 16:27:12 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test
 {
     public static void main(String[] args)
     {
-        Bank b=new Bank("SBI","Hyderabad",new Account[10]);
+        Bank b=new Bank("SBI","Hyderabad");
         System.out.println(b.addAccount(new SavingAccount(1005.00,5,10106,100000.00,"15/7/2023")));
         System.out.println(b.addAccount(new SavingAccount(1005.00,5,20202,1000000.00,"21/9/2020")));
         System.out.println(b.addAccount(new CurrentAccount(100000,30303,300000.00,"15/8/2016")));
